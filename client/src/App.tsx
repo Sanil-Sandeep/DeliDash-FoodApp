a5b96e8947934936a4c23cf942d673d55c4a65c9
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-11 22:43:39 +0530

    Build Login Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,37 @@
 import './App.css'
-import { Button } from './components/ui/button'
+import Login from './auth/Login'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import MainLayout from './layout/MainLayout'
+import Signup from './auth/Signup'
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<MainLayout/>,
+    // children:[
+    //   {
+    //     path:"/login"
+    //   }
+    // ]
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+  {
+    path:"/signup",
+    element:<Signup/>
+  },
+])
 
 function App() {
 
   return (
-    <>
-      <Button className='bg-orange hover:bg-hoverOrange'>Lets build food app</Button>
-    </>
+    <main>
+      <RouterProvider router={appRouter}>
+
+      </RouterProvider>
+    </main>
   )
 }
 
