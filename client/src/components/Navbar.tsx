6373e2329db35a5aa0f8c6b6f4c3a41fd866a2aa
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-15 18:45:22 +0530

    Update

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,37 @@
+import { Link } from "react-router-dom"
+import { Menubar, MenubarContent, MenubarTrigger } from "./ui/menubar";
+
 const Navbar = () => {
+  const admin = true;
   return (
-    <div>Navbar</div>
+    <div className="max-w-7xl mx-auto">
+      <div className="flex items-center justify-between h-14">
+        <Link to="/">
+          <h1 className="font-bold md:font-extrabold text-2xl">DeliDash</h1>
+        </Link>
+        <div className="hidden md:flex items-center gap-10">
+          <div className="hidden md:flex items-center gap-6">
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/order/status">Order</Link>
+          </div>
+          {
+            admin && (
+                        <Menubar>
+                          <MenubarTrigger>
+                            Dashboard
+                          </MenubarTrigger>
+                          <MenubarContent>
+                            <Link to="/admin/restaurant">Restaurant</Link>
+                            <Link to="/admin/menu">Menu</Link>
+                            <Link to="/admin/orders">Order</Link>
+                          </MenubarContent>
+                        </Menubar>
+                    )
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
