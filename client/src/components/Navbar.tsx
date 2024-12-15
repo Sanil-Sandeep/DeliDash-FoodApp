3a16fd5f6bf0c656854fcb98d4d3c2baa5c66402
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-15 19:52:40 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -22,77 +22,78 @@ const Navbar = () => {
             <Link to="/">Home</Link>
             <Link to="/profile">Profile</Link>
             <Link to="/order/status">Order</Link>
-          </div>
-          {
-            admin && (
-              <Menubar>
-                <MenubarMenu>
-                  <MenubarTrigger>
-                    Dashboard
-                  </MenubarTrigger>
-                  <MenubarContent>
-                    <Link to="/admin/restaurant">
-                      <MenubarItem>Restaurant</MenubarItem>
-                    </Link>
-                    <Link to="/admin/menu">
-                      <MenubarItem>Menu</MenubarItem>
-                    </Link>
-                    <Link to="/admin/orders">
-                      <MenubarItem>Orders</MenubarItem>
-                    </Link>
-                  </MenubarContent>
-                </MenubarMenu>
-              </Menubar>
-            )
-          }
-        </div>
-        <div className="flex items-center gap-4">
-          <div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem>
-                  Light
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  Dark
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          <Link to="/cart" className="relative cursor-pointer">
-            <ShoppingCart/>
-            <Button size={'icon'} className="absolute -inset-y-3 left-2 text-xs rounded-full h-4 w-4 bg-red-500 hover:bg-red-500">5</Button>
-          </Link>
-          <div>
-            <Avatar>
-              <AvatarImage/>
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
-          <div>
+
             {
-              loading ? (
-                <Button className="bg-orange hover:bg-hoverOrange">
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin"/>
-                  Please wait
-                </Button>
-              ) : (
-                <Button className="bg-orange hover:bg-hoverOrange">Logout</Button>
+              admin && (
+                <Menubar>
+                  <MenubarMenu>
+                    <MenubarTrigger>
+                      Dashboard
+                    </MenubarTrigger>
+                    <MenubarContent>
+                      <Link to="/admin/restaurant">
+                        <MenubarItem>Restaurant</MenubarItem>
+                      </Link>
+                      <Link to="/admin/menu">
+                        <MenubarItem>Menu</MenubarItem>
+                      </Link>
+                      <Link to="/admin/orders">
+                        <MenubarItem>Orders</MenubarItem>
+                      </Link>
+                    </MenubarContent>
+                  </MenubarMenu>
+                </Menubar>
               )
             }
-            
+          </div>
+          <div className="flex items-center gap-4">
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" size="icon">
+                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle theme</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem>
+                    Light
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Dark
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <Link to="/cart" className="relative cursor-pointer">
+              <ShoppingCart />
+              <Button size={'icon'} className="absolute -inset-y-3 left-2 text-xs rounded-full h-4 w-4 bg-red-500 hover:bg-red-500">5</Button>
+            </Link>
+            <div>
+              <Avatar>
+                <AvatarImage />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div>
+              {
+                loading ? (
+                  <Button className="bg-orange hover:bg-hoverOrange">
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    Please wait
+                  </Button>
+                ) : (
+                  <Button className="bg-orange hover:bg-hoverOrange">Logout</Button>
+                )
+              }
+
+            </div>
           </div>
         </div>
         <div className="md:hidden lg:hidden">
           {/* Mobile responsive */}
-            <MobileNavbar/>
+          <MobileNavbar />
         </div>
       </div>
     </div>
@@ -105,38 +106,38 @@ export default Navbar;
 const MobileNavbar = () => {
   return (
     <Sheet>
-    <SheetTrigger asChild>
-      <Button size={'icon'} className="rounded-full bg-gray-200 text-black hover:bg-gray-200" variant="outline">
-        <Menu/>
-      </Button>
-    </SheetTrigger>
-    <SheetContent>
-      <SheetHeader>
-        <SheetTitle>Edit profile</SheetTitle>
-        <SheetDescription>
-          Make changes to your profile here. Click save when you're done.
-        </SheetDescription>
-      </SheetHeader>
-      <div className="grid gap-4 py-4">
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
-            Name
-          </Label>
-          <Input id="name" value="Pedro Duarte" className="col-span-3" />
+      <SheetTrigger asChild>
+        <Button size={'icon'} className="rounded-full bg-gray-200 text-black hover:bg-gray-200" variant="outline">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
         </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="username" className="text-right">
-            Username
-          </Label>
-          <Input id="username" value="@peduarte" className="col-span-3" />
-        </div>
-      </div>
-      <SheetFooter>
-        <SheetClose asChild>
-          <Button type="submit">Save changes</Button>
-        </SheetClose>
-      </SheetFooter>
-    </SheetContent>
-  </Sheet>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   )
 }
