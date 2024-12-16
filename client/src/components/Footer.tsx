6eab93825c53bd6b8c2f2fe743cedaf153ec6518
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-16 20:18:57 +0530

    Update Footer.tsx

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,11 @@
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
-
-export default Footer
+    return (
+      <footer className="bg-gray-800 text-center text-gray-300 py-8 px-4">
+        <p className="text-sm">
+            &copy; 2024 PatelEats. All rights reserved.
+          </p> 
+      </footer>
+    );
+  };
+  
+  export default Footer;
