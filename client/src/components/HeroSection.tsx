82b3708bad52708f38091f3c2c143438ff760fae
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-16 19:32:25 +0530

    Update HeroSection.tsx

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -1,6 +1,12 @@
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className="flex flex-col md:flex-row max-w-7xl mx-auto md:p-10 rounded-lg items-center justify-center m-4 gap-20">
+        <div className="flex flex-col gap-10 md:w-[40%]">
+            <div className="font-bold md:font-extrabold md:text-5xl text-4xl">
+                <h1>Order Food anytime & anywhere</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
