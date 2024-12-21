90772405b34ff31e436b5b821c40d71c3fdc545e
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-21 17:43:54 +0530

    Update FilterPage.tsx

--- client/src/components/FilterPage.tsx
+++ client/src/components/FilterPage.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { Button } from './ui/button';
 
-type Props = {}
+export type FilterOptionsState = {
+  id:string;
+  label:string;
+};
 
-const FilterPage = (props: Props) => {
+const filterOptions: FilterOptionsState[] = [
+  {id:"burger", label:"Burger"},
+  {id:"biriyani", label:"Biriyani"},
+  {id:"pizza", label:"Pizza"},
+  {id:"rice", label:"Rice"},
+  {id:"noodles", label:"Noodles"},
+];
+
+const FilterPage = () => {
   return (
-    <div>FilterPage</div>
+    <div className='md:w-72'>
+      <div className='flex items-center justify-between'>
+        <h1 className='font-medium text-lg'>Filter by cuisines</h1>
+        <Button variant={"link"}>Reset</Button>
+      </div>
+      {
+        filterOptions.map((option) => (
+          <div key={option.id} className='flex items-center space-x-2 my-5'>
+            
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
