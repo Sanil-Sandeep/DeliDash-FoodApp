a727a5f13fdd7c4ac123f60ab8322008365555fc
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-21 18:34:19 +0530

    Update FilterPage.tsx

--- client/src/components/FilterPage.tsx
+++ client/src/components/FilterPage.tsx
@@ -1,4 +1,6 @@
 import { Button } from './ui/button';
+import { Checkbox } from './ui/checkbox';
+import { Label } from './ui/label';
 
 export type FilterOptionsState = {
   id:string;
@@ -14,6 +16,11 @@ const filterOptions: FilterOptionsState[] = [
 ];
 
 const FilterPage = () => {
+
+  const appliedFilterHandler = (value:string) => {
+    
+  };
+
   return (
     <div className='md:w-72'>
       <div className='flex items-center justify-between'>
@@ -23,7 +30,11 @@ const FilterPage = () => {
       {
         filterOptions.map((option) => (
           <div key={option.id} className='flex items-center space-x-2 my-5'>
-            
+              <Checkbox
+                id={option.id}
+                onClick={() => appliedFilterHandler(option.label)}
+              />
+              <Label className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'>{option.label}</Label>
           </div>
         ))
       }
