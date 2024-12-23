49f805085d52643d95f9b355adfa8ce22da73857
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-23 19:05:15 +0530

    Update RestauturantDetail.tsx

--- client/src/components/RestaurantDetail.tsx
+++ client/src/components/RestaurantDetail.tsx
@@ -1,7 +1,39 @@
+import Image from "@/assets/hero_pizza.png"
+import { Badge } from "./ui/badge"
+import { Timer } from "lucide-react"
+
 const RestaurantDetail = () => {
-  return (
-    <div>RestaurantDetail</div>
-  )
+    return (
+        <div className="max-w-6xl mx-auto my-10">
+            <div className="w-full">
+                <div className="relative w-full h-32 md:h64 lg:h-72">
+                    <img src={Image} alt="res_image" className="object-cover w-full h-full rounded-lg shadow-lg" />
+                </div>
+                <div className="flex flex-col md:flex-row justify-between">
+                    <div className="my-5">
+                        <h1 className="font-medium text-xl">Pizza Hut</h1>
+                        <div className="flex gap-2 my-2">
+                            {
+                                ["Spicy Fish Pizza", "Veggie Masala Pizza"].map((cuisine:string, idx:number) => (
+                                    <Badge key={idx}>{cuisine}</Badge>
+                                ))
+                            }
+                        </div>
+                        <div className="flex md:flex-row flex-col gap-2 my-5">
+                            <div className="flex items-center gap-2">
+                                <Timer className="w-5 h-5"/>
+                                <h1 className="flex items-center gap-2 font-medium">Delivery Time: {" "}
+                                    <span className="text-[#D19254]">
+                                        35 mins
+                                    </span>
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default RestaurantDetail
