ee6e2ac8d36ac3e16e32c0decd7807e758d5cc04
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-25 19:00:37 +0530

    update

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -143,7 +143,7 @@ const MobileNavbar = () => {
             <HandPlatter />
             <span>Order</span>
           </Link>
-          <Link to="/profile" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
+          <Link to="/cart" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
             <ShoppingCart />
             <span>Cart (0)</span>
           </Link>
