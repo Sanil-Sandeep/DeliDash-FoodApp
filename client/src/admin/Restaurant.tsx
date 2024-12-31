4e9da937486b9abc78fbf77dfd858252051843c1
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-31 18:42:20 +0530

    update

--- client/src/admin/Restaurant.tsx
+++ client/src/admin/Restaurant.tsx
@@ -1,6 +1,20 @@
 const Restaurant = () => {
   return (
-    <div>Restaurant</div>
+    <div className="max-w-6xl mx-auto my-10">
+      <div>
+        <div>
+          <h1 className="font-extrabold text-2xl mb-5">Add Restaurants</h1>
+          <form action="">
+            <div className="md:grid grid-cols-2 gap-6 space-y-2 md:space-y-0">
+              {/* Restaurant Name */}
+              <div>
+                
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
