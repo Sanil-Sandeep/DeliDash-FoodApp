eb11a8def82a7066d761c0e06518114bd3a6c743
Sanil Sandeep <sanil.sandeep001@gmail.com>
2025-01-01 19:37:33 +0530

    Update Restaurant,tsx

--- client/src/admin/Restaurant.tsx
+++ client/src/admin/Restaurant.tsx
@@ -1,4 +1,10 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Loader2 } from "lucide-react";
+
 const Restaurant = () => {
+  const loading = false;
   return (
     <div className="max-w-6xl mx-auto my-10">
       <div>
@@ -8,8 +14,65 @@ const Restaurant = () => {
             <div className="md:grid grid-cols-2 gap-6 space-y-2 md:space-y-0">
               {/* Restaurant Name */}
               <div>
-                
+                <Label>Restaurant Name</Label>
+                <Input 
+                  type="text"
+                  name="restaurantName"
+                  placeholder="Enter your restaurant name"
+                />
               </div>
+              <div>
+                <Label>City</Label>
+                <Input 
+                  type="text"
+                  name="city"
+                  placeholder="Enter your city name"
+                />
+              </div>
+              <div>
+                <Label>Country</Label>
+                <Input 
+                  type="text"
+                  name="country"
+                  placeholder="Enter your country name"
+                />
+              </div>
+              <div>
+                <Label>Delivery Time</Label>
+                <Input 
+                  type="text"
+                  name="deliveryTime"
+                  placeholder="Enter your restaurant name"
+                />
+              </div>
+              <div>
+                <Label>Cuisines</Label>
+                <Input 
+                  type="text"
+                  name="cuisines"
+                  placeholder="Enter your cuisimes"
+                />
+              </div>
+              <div>
+                <Label>Upload Restaurant Banner</Label>
+                <Input 
+                  type="file"
+                  accept="image/*"
+                  name="image"
+                />
+              </div>
+            </div>
+            <div>
+              {
+                loading ? (
+                  <Button disabled className="bg-orange hover:bg-hoverOrange">
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin"/>
+                    Add Your Restaurant
+                  </Button>
+                ) : (
+                  <Button className="bg-orange hover:bg-hoverOrange">Add Your Restaurant</Button>
+                )
+              }
             </div>
           </form>
         </div>
