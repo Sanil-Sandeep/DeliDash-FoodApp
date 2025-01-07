857269e3eabacc42ecd283ee035495a127d4a41b
Sanil Sandeep <sanil.sandeep001@gmail.com>
2025-01-07 19:31:42 +0530

    Update Orders.tsx

--- client/src/admin/Orders.tsx
+++ client/src/admin/Orders.tsx
@@ -1,4 +1,5 @@
 import { Label } from "@/components/ui/label"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 const Orders = () => {
     return (
@@ -8,7 +9,7 @@ const Orders = () => {
                 {/* Restaurant Orders display here */}
                 <div className="flex flex-col md:flex-row justify-between items-start sm:items-center bg-white dark:bg-gray-800 shadow-lg rounded-xl p-6 sm:p-8 border-gray-200 dark:border-gray-700">
                     <div className="flex-1 mb-6 sm:mb-0">
-                        <h1 className="text-xl font-semibold text-gray-800 dark:text-gray-100">Lorem</h1>
+                        <h1 className="text-xl font-semibold text-gray-800 dark:text-gray-100">Sanil Sandeep</h1>
                         <p className="text-gray-600 dark:text-gray-400 mt-2">
                             <span className="font-semibold">Address: </span>
                             Lorem, ipsum dolor.
@@ -20,7 +21,20 @@ const Orders = () => {
                     </div>
                     <div className="w-full sm:w-1/3">
                         <Label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Order Status</Label>
-                        
+                        <Select>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Select Status"/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    {
+                                        ["Pending", "Confirmed", "Preparing", "OutForDelivery", "Delivered"].map((status:string, index:number) => (
+                                            <SelectItem key={index} value={status.toLowerCase()}>{status}</SelectItem>
+                                        ))
+                                    }
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
                     </div>
                 </div>
             </div>
