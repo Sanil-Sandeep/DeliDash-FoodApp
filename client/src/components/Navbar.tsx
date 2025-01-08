5c38c95f2585f2bed3c12abb3cf1c6ef03b425f3
Sanil Sandeep <sanil.sandeep001@gmail.com>
2025-01-08 18:45:18 +0530

    Update

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -139,7 +139,7 @@ const MobileNavbar = () => {
             <User />
             <span>Profile</span>
           </Link>
-          <Link to="/order" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
+          <Link to="/order/status" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
             <HandPlatter />
             <span>Order</span>
           </Link>
